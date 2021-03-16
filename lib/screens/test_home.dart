0824b84d937dacab8f6af95cd41192e5008e09b9
Sharib Ahmed <sharib@webmesecure.com>
2021-03-17 01:41:11 +0530

    added app bar +changed latest relaese ui

---
+++ lib/screens/test_home.dart
@@ -0,0 +1,166 @@
+//libraries
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+//packages
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+//screens
+import 'downloads.dart';
+import 'search_result.dart';
+//widgets
+import '../widgets/latest_release_home.dart';
+//provider
+import '../provider/http_calls.dart';
+
+class HomeTest extends StatefulWidget {
+  @override
+  _HomeTestState createState() => _HomeTestState();
+}
+
+class _HomeTestState extends State<HomeTest> {
+  final ctrl = TextEditingController();
+
+  var page = 1;
+  var _boolCheck1 = true;
+  var _loading1 = true;
+  @override
+  void dispose() {
+    super.dispose();
+    ctrl.dispose();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (_boolCheck1) {
+      print('fetching latest release');
+      Provider.of<HttpCalls>(context).showLatestRelease(page).then((_) {
+        setState(() {
+          _loading1 = false;
+        });
+      });
+    }
+    _boolCheck1 = false;
+  }
+
+  Widget _searchTextField() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10.0, top: 10, right: 10),
+      child: TextField(
+          controller: ctrl,
+          decoration: InputDecoration(
+            hintText: 'Search Here',
+            prefixIcon: const Icon(CupertinoIcons.search),
+            enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide: const BorderSide(
+                  color: Colors.black,
+                )),
+            focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide: const BorderSide(
+                  color: Colors.black,
+                )),
+          )),
+    );
+  }
+
+  Widget _searchButton() {
+    return Padding(
+      padding: const EdgeInsets.only(right: 10.0),
+      child: Align(
+        alignment: Alignment.bottomRight,
+        child: ElevatedButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
+          ),
+          onPressed: () {
+            if (ctrl.text.isNotEmpty) {
+              FocusScope.of(context).unfocus();
+              Navigator.pushNamed(context, SearchResult.routeName,
+                  arguments: ctrl.text);
+            }
+          },
+          child: const Text('Search'),
+        ),
+      ),
+    );
+  }
+
+  Widget _latestReleaseHome() {
+    return Padding(
+        padding: const EdgeInsets.only(left: 8.0, top: 8),
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: const BorderRadius.only(
+                  topLeft: const Radius.circular(5),
+                  bottomLeft: const Radius.circular(5)),
+              color: Colors.black12),
+          height: 200,
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: Text('Latest Release(SUB)',
+                    style: GoogleFonts.openSans(
+                        decoration: TextDecoration.underline,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20)),
+              ),
+              Consumer<HttpCalls>(
+                builder: (context, httpCallsObject, _) => _loading1
+                    ? const Center(
+                        child: const Text('Fetching Data, please wait...'))
+                    : Expanded(
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context, index) => Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: LatestReleaseHome(
+                                latestRelease:
+                                    httpCallsObject.latestRelease[index],
+                                lazyLoading:
+                                    _loading1), //will use this lazy loading to shimmer effect
+                          ),
+                          itemCount: httpCallsObject.latestRelease.length,
+                        ),
+                      ),
+              )
+            ],
+          ),
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.black,
+        leading: Image.asset('android/app/src/main/res/drawable/icon.png'),
+        title: Text(
+          'Stream/Download Anime',
+          style: GoogleFonts.montserrat(),
+        ),
+      ),
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.pushNamed(context, DownloadScreen.routeName);
+          },
+          child: const Icon(Icons.file_download)),
+      resizeToAvoidBottomInset: false,
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [_searchTextField(), _searchButton(), _latestReleaseHome()],
+        ),
+      ),
+    ));
+  }
+}
---
+++ lib/widgets/latest_release_home.dart
@@ -0,0 +1,67 @@
+import 'package:anime_shows_android/model/latest_model.dart';
+import 'package:flutter/material.dart';
+
+class LatestReleaseHome extends StatelessWidget {
+  final LatestModel latestRelease;
+  final bool lazyLoading;
+  LatestReleaseHome({this.latestRelease, this.lazyLoading});
+  Widget _animePoster() {
+    return Card(
+      elevation: 50,
+      shadowColor: Colors.black,
+      child: Image.network(
+        latestRelease.poster,
+        fit: BoxFit.contain,
+      ),
+    );
+  }
+
+  Widget _animeDetails() {
+    return Positioned(
+      bottom: 3,
+      left: 3,
+      right: 3.5,
+      child: Container(
+        decoration: BoxDecoration(color: Colors.black38),
+        child: Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              RichText(
+                text: TextSpan(
+                    text: 'Title: ',
+                    style: const TextStyle(fontWeight: FontWeight.w600),
+                    children: [
+                      TextSpan(
+                          text: latestRelease.name,
+                          style: const TextStyle(fontWeight: FontWeight.w400))
+                    ]),
+              ),
+              RichText(
+                text: TextSpan(
+                    text: 'Episode: ',
+                    style: const TextStyle(fontWeight: FontWeight.w600),
+                    children: [
+                      TextSpan(
+                          text: latestRelease.episode,
+                          style: const TextStyle(fontWeight: FontWeight.w400))
+                    ]),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        _animePoster(),
+        _animeDetails(),
+      ],
+    );
+  }
+}
